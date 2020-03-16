8448727ae823ad676e179e674a5ff221d9940f15
Farrel Ceskicevas <farrelceskicevas@gmail.com>
2020-03-16 11:07:28 +0700

    Menambahkan primitif pada file List-Parent-Reviewer.h

--- Movie Reviewer APPs/List-Parent-Reviewer.h
+++ Movie Reviewer APPs/List-Parent-Reviewer.h
@@ -2,4 +2,16 @@
 #define LIST-PARENT-REVIEWER_H_INCLUDED
 #include "General-Header.h"
 
+void createListUser(List_User &L);
+bool isEmptyListUser(List_User &L);
+address_User createElmUser(string username, string password, int umur);
+void viewListUser(List_User L);
+void insertLastUser(List_User &L, address_User P);
+void deleteFirstUser(List_User &L, address_User &P);
+void deleteLastUser(List_User &L, address_User &P);
+void deleteAfterUser(address_User Prec, address_User &P);
+address_User searchElmUser(List_User L, string username);
+void deleteElmUser(List_User &L, string username);
+void loadDataUsers(List_User &L);
+
 #endif // LIST-PARENT-REVIEWER_H_INCLUDED
